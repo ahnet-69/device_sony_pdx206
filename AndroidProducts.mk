f9b7260b75bc8cb3d1d5f02129b3300ea28df00c
realahnet <syedmusicrealise@gmail.com>
2025-12-01 12:15:49 +0500

    pdx206: Adapt to Statix

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pdx206.mk
+    $(LOCAL_DIR)/statix_pdx206.mk
--- statix_pdx206.mk
+++ statix_pdx206.mk
@@ -21,12 +21,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.mk
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common StatiX stuff.
+$(call inherit-product, vendor/statix/config/common.mk)
+$(call inherit-product, vendor/statix/config/gsm.mk)
+
+# Pixel Launcher
+INCLUDE_PIXEL_LAUNCHER := true
 
 IS_PHONE := true
 
-PRODUCT_NAME := lineage_pdx206
+PRODUCT_NAME := statix_pdx206
 PRODUCT_DEVICE := pdx206
 PRODUCT_MANUFACTURER := Sony
 PRODUCT_BRAND := Sony
